25e56ca435ff07e00b7f2c53eab5c1a8dcd25b49
DRTX2 <davidmanjarres2004@gmail.com>
2025-07-07 01:58:43 -0500

    Add logs to controller and UseCase for user module.

--- back/src/main/java/drtx/cleanArchitecture/example/infrastructure/usecase/UserUseCaseImpl.java
+++ back/src/main/java/drtx/cleanArchitecture/example/infrastructure/usecase/UserUseCaseImpl.java
@@ -13,6 +13,8 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.service.spi.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,10 @@ import java.util.Objects;
 @Service                               // Solo en infraestructure
 @RequiredArgsConstructor
 public class UserUseCaseImpl implements UserUseCase {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserUseCaseImpl.class); //Simple Logging Facade for Java
+//                                                                clase a la que pertenece el logger,
+//                                                                para asociarlo, mostrarlo en logs, permitir config especifica,
+//                                                                tambien asi se consigue logs con niveles
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final UserCreateMapper userCreateMapper;
@@ -51,6 +56,7 @@ public class UserUseCaseImpl implements UserUseCase {
     @Override
     @Transactional
     public UserDto createUser(UserCreateDto userDTO) {
+        logger.info("Creating user {}", userDTO);// logger replace {}, with the dynamic parameter
         Objects.requireNonNull(userDTO, "User data cannot be null");
 
         User user = userCreateMapper.toEntity(userDTO);
@@ -62,14 +68,18 @@ public class UserUseCaseImpl implements UserUseCase {
 
         try {
             User saved = userRepository.save(user);
-            return userMapper.toDto(saved);
+            UserDto userSavedDto= userMapper.toDto(saved);
+            logger.debug("Created user with id {}", userSavedDto.id());
+            return userSavedDto;
         } catch (DataIntegrityViolationException ex) {
             if (ex.getCause() instanceof ConstraintViolationException) {
                 throw new EmailAlreadyExistsException("Email already exists: " + user.getEmail());
             }
             throw new ServiceException("Error saving user", ex);
         } catch (Exception ex) {
-            throw new ServiceException("Unexpected error creating user", ex);
+            String errorMessage = "Unexpected error creating user: " + ex.getMessage();
+            logger.error(errorMessage);
+            throw new ServiceException(errorMessage, ex);
         }
     }
 
--- back/src/main/java/drtx/cleanArchitecture/example/infrastructure/web/UserController.java
+++ back/src/main/java/drtx/cleanArchitecture/example/infrastructure/web/UserController.java
@@ -5,6 +5,8 @@ import drtx.cleanArchitecture.example.application.dto.UserDto;
 import drtx.cleanArchitecture.example.infrastructure.usecase.UserUseCaseImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,31 +16,36 @@ import java.util.List;
 // @CrossOrigin(origins = "http://localhost:5173/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserUseCaseImpl userService;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    private final UserUseCaseImpl userUseCase;
 
     @GetMapping
     public List<UserDto> getUsers(){
-        return  userService.findAllUsers();
+        return  userUseCase.findAllUsers();
     }
 
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable Long id) {
-        return userService.getUserById(id);
+        log.info("GET /users/{}", id);
+        var dto = userUseCase.getUserById(id);
+        log.info("Respuesta: {}", dto);
+        return dto;
     }
 
     @PostMapping
     public UserDto createUser(@Valid @RequestBody UserCreateDto userDTO) { // de paso se valida q no sea nulo
-        return userService.createUser(userDTO);
+        return userUseCase.createUser(userDTO);
     }
 
     @PutMapping("/{idUser}")
     public UserDto updateUser(@PathVariable Long idUser, @Valid @RequestBody UserCreateDto userDto){
-        return userService.updateUser(idUser, userDto);
+        return userUseCase.updateUser(idUser, userDto);
     }
 
     @DeleteMapping("/{id}")
     public UserDto deleteUser(@PathVariable Long id){
-        return userService.deleteUser(id);
+        return userUseCase.deleteUser(id);
     }
 
 }
